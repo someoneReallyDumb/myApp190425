acd9f478df7322f324937fc1981fc64228ab08bd
Ilya <viosagmir@gmail.com>
2025-04-19 11:59:43 +0300

    ChangeOperation

--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@
         int a = int.Parse(Console.ReadLine());
         int b = int.Parse(Console.ReadLine());
 
-        System.Console.WriteLine(a + b);
+        System.Console.WriteLine(a - b);
     }
 }
