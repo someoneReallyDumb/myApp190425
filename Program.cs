21bd12307f681c0ced73bf112bb6a2c867dc64f0
Ilya <viosagmir@gmail.com>
2025-04-19 11:57:50 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine(a + b);
+    }
+}
